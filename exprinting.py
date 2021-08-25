f1608ff97913c830d9f9a21c689942deda208344
mmajzner <89032314+mmajzner@users.noreply.github.com>
2021-08-25 13:30:30 -0500

    Add files via upload

---
+++ exprinting.py
@@ -0,0 +1,21 @@
+"""
+    Program 1: printing a song or poem
+"""
+
+print("I'm not loving you, way I wanted to")
+print("What I had to do, had to rub from you")
+print("I'm in love with you, but the vibe is wrong")
+print("And that haunted me, all the way home")
+print("So you never know, never, never, never know")
+print("Never know enough, 'til it's over love")
+print("'Til we lose control, system overload")
+print("Screaming, No, no, no, n-nooo!")
+print("I'm not loving you, way I wanted to")
+print("See I wanna move, but can't escape from you")
+print("So I keep it low, keep a secret code")
+print("So everybody else don't have to know\n")
+
+print("So keep your love lockdown-your love lockdown")
+print("Keeping your love lockdown-your love lockdown")
+print("Now keep your love lockdown-your love lockdown")
+print("Now keep your love lockdown-you lose")
