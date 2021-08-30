0c0df4ea03f0aa488fac1eca489c9e87703cde9a
mmajzner <89032314+mmajzner@users.noreply.github.com>
2021-08-30 03:18:37 -0500

    Add files via upload

---
+++ Practice2.1.py
@@ -0,0 +1,36 @@
+"""
+Copy your initial program from Programming Practice 1.1 into a new .py file.
+(This was the poem or song that you printed to the screen.) Modify the file
+to accept input from the user, assign the input to a minimum of 8 variables,
+and use the variables in the output. You are essentially creating a mad-libs
+game.
+
+Turn in your modified file that accepts input from the user, assigns the
+input to a minimum of 8 variables, and uses the variables in the output which
+essentially creates a mad-libs game.
+"""
+
+name = input('Enter a pronoun: ')
+action = input('Enter a verb: ')
+verb = input('Enter a verb: ')
+verb2 = input('Enter the same verb with an "ing": ')
+pronoun = input('Enter a pronoun: ')
+adverb = input('Enter a adverb: ')
+
+print(pronoun + "'m not " + verb2 + name + ", way " + pronoun + " wanted to")
+print("What " + pronoun + " had to do, had to run from " + name)
+print(pronoun + "'m in " + action + " with " + name + ", but the vibe is wrong")
+print("And that haunted me, all the way home")
+print("So " + name + adverb + " know, " + adverb + ", " + adverb + " know")
+print(adverb + " know enough, 'til it's over " + action)
+print("'Til we lose control, system overload")
+print("Screaming, 'No, no, no, n-nooo!'")
+print(pronoun + "'m not " + verb2 + name + ", way " + pronoun + " wanted to")
+print("See " + pronoun + " wanna move, but can't escape from " + name)
+print("So " + pronoun + verb + " it low, " + verb + " a secret code")
+print("So everybody else don't have to know.")
+
+print("So " + verb + " your " + action + " lockdown—your " + action + " lockdown")
+print(verb + "ing your " + action + " lockdown—your " + action + " lockdown")
+print("Now " + verb + " your " + action + " lockdown—your " + action + " lockdown")
+print("Now " + verb + " your " + action + " lockdown—you lose.\n")
